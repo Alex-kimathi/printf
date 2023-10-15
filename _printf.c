4e5674b80325e20941002a9556fc9ea5ebbe3421
Alex-kimathi <alex2kimathi@gmail.com>
2023-10-15 14:52:12 +0000

    Printf

--- _printf.c
+++ _printf.c
@@ -1,14 +1,15 @@
 #include "main.h"
-<<<<<<< HEAD
-
 /**
  * _printf - a function to produce formatted
  * output to stdout
+ * @c: handles char
+ * @%: handles the sign
+ * @s: handles a string
  *
  * @format: format specifier
  * Return: the number of characters printed
  */
-
+/*
 int _printf(const char *format, ...)
 {
 	unsigned int i, s_count;
@@ -21,18 +22,18 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] != '%')
 		{
-			writec(format[i]);    /*bado sijadefine function ya kuwrite char*/
+			writec(format[i]);
 		}
 		else if (format[i + 1] == 'c')
 		{
-			writec(va_arg(args, int));   /*see above*/
+			writec(va_arg(args, int));
 			i++;
 		}
 		else if (format[i + 1] == 's')
 		{
-			s_count = writes(va_arg(args, char *));  /* bado sijadefine function ya kuwrite string*/
+			s_count = writes(va_arg(args, char *));
 			i++;
-			count += (s_count - 1);   /* this was a little confusing */
+			count += (s_count - 1);
 		}
 		else if (format[i + 1] == '%')
 		{
@@ -44,5 +45,45 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (count);
 }
-=======
->>>>>>> 86ea7072fd11501fe6a1a38f79a3fd4f7b207447
+*/
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list args;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			count++;
+		}
+		else
+		{
+			format++;
+			if (*format == '/0')
+				break;
+			if (*format == '%')
+				write(1, format, 1);
+			else if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+
+				write(1, &c, 1);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
+
+				write(1, str, strlen(str));
+				count = count + strlen(str);
+			}
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
